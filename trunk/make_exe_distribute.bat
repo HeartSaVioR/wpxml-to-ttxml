b3ab80e20c32c04f03b2ce13042ecebe48110aaa
kabhwan <kabhwan@73988b76-2849-11de-a7ed-71eef27883a5>
2009-04-15 08:42:47 +0000

    * 배포 방법 변경   - Tar2RubyScript 없어도 바이너리 생성 가능함   - make_exe_distribute.bat 로 만들고 dist-clean.bat 으로 중간 파일들을 정리한다

--- trunk/make_exe_distribute.bat
+++ trunk/make_exe_distribute.bat
@@ -1,7 +1,5 @@
-REM 이 파일을 실행하기 전에 make_archived_source.bat 를 실행하여 
-REM dist 디렉토리에 WPXML2TTXML.rb 파일이 생성된 상태에서 진행하시기 바랍니다.
-
 @echo off
-cd dist
 del WPXML2TTXML.exe
-rubyscript2exe WPXML2TTXML.rb WPXML2TTXML.exe
+mkdir WPXML2TTXML
+copy *.rb WPXML2TTXML\*
+rubyscript2exe WPXML2TTXML\
